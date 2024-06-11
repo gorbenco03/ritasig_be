a18b0fda7488c1d42c3e052e502b6b6e56de2679
gorbenco03 <chiril.gorbenco@student.upt.ro>
2024-06-11 23:40:23 +0300

    last

--- app.js
+++ app.js
@@ -1,44 +1,36 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-// Conexiunea la baza de date
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+const { v4: uuidv4 } = require('uuid');
+const indexRouter = require('./routes/index');
+const Insurance = require('./models/Insurance');
+const usersRouter = require('./routes/users');
 const connectDB = require('./db');
 connectDB();
 
-var app = express();
+const app = express();
 
-// Setări pentru view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// Middleware-uri standard
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors()); // Permite cereri cross-origin
+app.use(cors());
 
-// Rute principale
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// Importarea modelelor
-const Insurance = require('./models/Insurance');
-
-// Ruta pentru testarea funcționării serverului
 app.get('/api/test', (req, res) => {
   res.status(200).send({ message: 'Serverul funcționează corect!' });
 });
 
-// Endpoint pentru adăugarea unei asigurări
 app.post('/api/insurance', async (req, res) => {
   try {
     const newInsurance = new Insurance(req.body);
@@ -50,7 +42,182 @@ app.post('/api/insurance', async (req, res) => {
   }
 });
 
-// Endpoint pentru a prelua toate asigurările
+app.patch('/api/insurances/:id/status', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { status } = req.body;
+    const insurance = await Insurance.findById(id);
+
+    if (!insurance) {
+      return res.status(404).send('Asigurarea nu a fost găsită.');
+    }
+
+    if (status === 'Acceptat') {
+      const tomorrow = new Date();
+      tomorrow.setDate(tomorrow.getDate() + 1);
+
+      let endDate = new Date(tomorrow);
+      if (insurance.PerioadaDeAsigurare === '1 an') {
+        endDate.setFullYear(endDate.getFullYear() + 1);
+      } else if (insurance.PerioadaDeAsigurare === '6 luni') {
+        endDate.setMonth(endDate.getMonth() + 6);
+      } else if (insurance.PerioadaDeAsigurare === '3 luni') {
+        endDate.setMonth(endDate.getMonth() + 3);
+      }
+
+      insurance.DataInceput = tomorrow;
+      insurance.DataSfarsit = endDate;
+      let prefix;
+      switch (insurance.TipAsigurare) {
+        case 'RCA':
+          prefix = 'RCA-';
+          break;
+        case 'CASCO':
+          prefix = 'CAS-';
+          break;
+        case 'CARTE VERDE':
+          prefix = 'CARV-';
+          break;
+        default:
+          prefix = '';
+      }
+      insurance.NumarSerie = prefix + uuidv4();
+    }
+
+    insurance.StatusAsigurare = status;
+    await insurance.save();
+
+    if (status === 'Acceptat') {
+      let templatePath = path.join(__dirname, 'template.html');
+      if (insurance.TipAsigurare === 'CARTE VERDE') {
+        templatePath = path.join(__dirname, 'templateCV.html');
+      }
+      const template = fs.readFileSync(templatePath, 'utf8');
+
+      const replacements = {
+        '{Nume}': insurance.Nume,
+        '{Prenume}': insurance.Prenume,
+        '{CNP}': insurance.CNP,
+        '{DataNasterii}': insurance.DataNasterii,
+        '{PermisDeConducere}': insurance.PermisDeConducere,
+        '{Email}': insurance.Email,
+        '{NrDeTelefon}': insurance.NrDeTelefon,
+        '{CategorieBonus}': insurance.CategorieBonus,
+        '{CategoriaMasinii}': insurance.CategoriaMasinii,
+        '{NumarulWIN}': insurance.NumarulWIN,
+        '{AnFabricatie}': insurance.AnProductie,
+        '{CapacitateCilindrica}': insurance.CapacitateCilindrica,
+        '{NrInmatriculare}': insurance.NrInmatriculare,
+        '{NrCertificatInmatriculare}': insurance.NrCertificatInmatriculare,
+        '{NormaDePoluare}': insurance.NormaDePoluare,
+        '{TipCombustibil}': insurance.TipCombustibil,
+        '{PerioadaDeAsigurare}': insurance.PerioadaDeAsigurare,
+        '{TipAsigurare}': insurance.TipAsigurare,
+        '{PretAsigurare}': insurance.PretAsigurare,
+        '{Adresa}': insurance.Adresa,
+        '{Seria}': insurance.NumarSerie,
+        '{DataInceput}': insurance.DataInceput,
+        '{DataSfarsit}': insurance.DataSfarsit,
+        '{MarcaMasinii}': insurance.MarcaMasinii,
+        '{Modelul}': insurance.Modelul,
+      };
+
+      let htmlContent = template;
+      for (const [key, value] of Object.entries(replacements)) {
+        const regex = new RegExp(key, 'g');
+        htmlContent = htmlContent.replace(regex, value);
+      }
+
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.setContent(htmlContent);
+      const pdfPath = path.join(__dirname, 'public', `Detalii-Asigurare-${insurance._id}.pdf`);
+      await page.pdf({ path: pdfPath, format: 'A4' });
+
+      await browser.close();
+
+      console.log('PDF created successfully.');
+    }
+
+    res.send(insurance);
+  } catch (error) {
+    res.status(500).send('Eroare la server: ' + error.message);
+  }
+});
+app.post('/api/insurances/:id/generate-invoice', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const insurance = await Insurance.findById(id);
+    if (!insurance) {
+      return res.status(404).send('Asigurarea nu a fost găsită.');
+    }
+
+    const template = fs.readFileSync(path.join(__dirname, 'templateFactura.html'), 'utf8');
+
+    const replacements = {
+      '{Nume}': insurance.Nume,
+      '{Prenume}': insurance.Prenume,
+      '{CNP}': insurance.CNP,
+      '{DataNasterii}': insurance.DataNasterii,
+      '{PermisDeConducere}': insurance.PermisDeConducere,
+      '{Email}': insurance.Email,
+      '{NrDeTelefon}': insurance.NrDeTelefon,
+      '{CategorieBonus}': insurance.CategorieBonus,
+      '{CategoriaMasinii}': insurance.CategoriaMasinii,
+      '{NumarulWIN}': insurance.NumarulWIN,
+      '{AnFabricatie}': insurance.AnProductie,
+      '{CapacitateCilindrica}': insurance.CapacitateCilindrica,
+      '{NrInmatriculare}': insurance.NrInmatriculare,
+      '{NrCertificatInmatriculare}': insurance.NrCertificatInmatriculare,
+      '{NormaDePoluare}': insurance.NormaDePoluare,
+      '{TipCombustibil}': insurance.TipCombustibil,
+      '{PerioadaDeAsigurare}': insurance.PerioadaDeAsigurare,
+      '{TipAsigurare}': insurance.TipAsigurare,
+      '{PretAsigurare}': insurance.PretAsigurare,
+      '{Adresa}': insurance.Adresa,
+      '{Seria}': insurance.NumarSerie,
+      '{DataInceput}': insurance.DataInceput,
+      '{DataSfarsit}': insurance.DataSfarsit,
+      '{MarcaMasinii}': insurance.MarcaMasinii,
+      '{Modelul}': insurance.Modelul,
+    };
+
+    let htmlContent = template;
+    for (const [key, value] of Object.entries(replacements)) {
+      const regex = new RegExp(key, 'g');
+      htmlContent = htmlContent.replace(regex, value);
+    }
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(htmlContent);
+    const pdfPath = path.join(__dirname, 'public', `Factura-${insurance._id}.pdf`);
+    await page.pdf({ path: pdfPath, format: 'A4' });
+
+    await browser.close();
+
+    res.json({ pdfUrl: `/public/Factura-${insurance._id}.pdf` });
+  } catch (error) {
+    res.status(500).send('Eroare la server: ' + error.message);
+  }
+});
+
+app.get('/api/insurances/:id/download-pdf', (req, res) => {
+  const { id } = req.params;
+  const pdfPath = path.join(__dirname, 'public', `Detalii-Asigurare-${id}.pdf`);
+
+  if (fs.existsSync(pdfPath)) {
+    res.download(pdfPath, `Detalii-Asigurare-${id}.pdf`, (err) => {
+      if (err) {
+        console.error('Error downloading PDF:', err);
+        res.status(500).send('Error downloading PDF');
+      }
+    });
+  } else {
+    res.status(404).send('PDF not found');
+  }
+});
+
 app.get('/api/insurances', async (req, res) => {
   try {
     const insurances = await Insurance.find({});
@@ -61,45 +228,18 @@ app.get('/api/insurances', async (req, res) => {
   }
 });
 
-// Actualizarea statusului asigurărilor
-app.patch('/api/insurances/:id/status', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { status } = req.body; // Acceptat sau Respins
-    const insurance = await Insurance.findByIdAndUpdate(id, { StatusAsigurare: status }, { new: true });
-    console.log(id)
-    if (!insurance) {
-      return res.status(404).send('Asigurarea nu a fost găsită.');
-    }
-
-    // Adaugă aici logica pentru generarea PDF-ului, dacă este necesar
-    if (status === 'Acceptat') {
-      // Logică pentru generarea PDF-ului aici
-    }
-
-    res.send(insurance);
-  } catch (error) {
-    res.status(500).send('Eroare la server: ' + error.message);
-  }
-});
-
-// Middleware pentru gestionarea erorilor 404
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.status(404).send('Not Found');
 });
 
-// Handler de erori
 app.use(function(err, req, res, next) {
-  // Setează locale și oferă eroare în development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // Răspunde cu pagina de eroare
   res.status(err.status || 500);
   res.render('error');
 });
 
-// Pornirea serverului
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
--- models/Insurance.js
+++ models/Insurance.js
@@ -1,30 +1,32 @@
-// models/Insurance.js
-
 const mongoose = require('mongoose');
 
 const InsuranceSchema = new mongoose.Schema({
-  Nume: String,
-  Prenume: String,
-  CNP: Number,
-  DataNasterii: Date,
-  PermisDeConducere: String,
-  Email: String,
-  NrDeTelefon: String,
-  CategorieBonus: Number,
-  CategoriaMasinii: String,
-  NumarulWIN: String,
-  AnProductie: Number,
-  CapacitateCilindrica: Number,
-  NrInmatriculare: String,
-  NrCertificatInmatriculare: String,
-  NormaDePoluare: String,
-  TipCombustibil: String,
-  CategorieBonus: Number,
-  PerioadaDeAsigurare: String,
-  TipAsigurare: String,
-  PretAsigurare: Number, 
-  StatusAsigurare: String, 
-  
+  Nume: { type: String, required: true },
+  Prenume: { type: String, required: true },
+  CNP: { type: Number, required: true },
+  DataNasterii: { type: Date, required: true },
+  PermisDeConducere: { type: String, required: true },
+  Email: { type: String, required: true },
+  NrDeTelefon: { type: String, required: true },
+  Adresa: { type: String, required: true },
+  CategorieBonus: { type: Number, required: true },
+  MarcaMasinii: { type: String, required: true },
+  Modelul: { type: String, required: true },
+  CategoriaMasinii: { type: String, required: true },
+  NumarulWIN: { type: String, required: true },
+  AnProductie: { type: Number, required: true },
+  CapacitateCilindrica: { type: Number, required: true },
+  NrInmatriculare: { type: String, required: true },
+  NrCertificatInmatriculare: { type: String, required: true },
+  NormaDePoluare: { type: String, required: true },
+  TipCombustibil: { type: String, required: true },
+  PerioadaDeAsigurare: { type: String, required: true },
+  TipAsigurare: { type: String, required: true },
+  PretAsigurare: { type: Number, required: true },
+  StatusAsigurare: { type: String, required: false },
+  NumarSerie: { type: String, required: true },
+  DataInceput: { type: Date, required: false },
+  DataSfarsit: { type: Date, required: false }
 });
 
 module.exports = mongoose.model('Insurance', InsuranceSchema);
